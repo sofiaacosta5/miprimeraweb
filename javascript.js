1ed3157515f709dfd1218e90eb1902b8b14af4ed
sofiaacosta5 <166668826+sofiaacosta5@users.noreply.github.com>
2024-05-19 18:36:31 -0300

    probandojavascript

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let nombre = "nombre";
+let apellido = "apellido";
+let resultado = nombre + apellido;
+console.log(resultado);
+let numberone = 5;
+let numbertwo = 3;
+let resultsuma = numberone + numbertwo;
+console.log("suma:",resultsuma);
+let numberthree = resultsuma + 2;
+let resultsumados = resultsuma + 2;
+console.log("suma:",resultsumados);
+
+
